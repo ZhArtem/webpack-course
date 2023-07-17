c4a41666483ca085b49ea3ca39449e6633be669e
Artem Zhoglo <zhogloaa@yandex.ru>
2023-07-17 20:04:16 +0300

    Added xml loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Post from './Post'
 import json from './assets/json'
+import xml from './assets/data.xml'
 import WebpackLogo from './assets/webpack-logo.png'
 import './styles/styles.css'
 
@@ -9,3 +10,4 @@ const post = new Post('Webpack Post Title', WebpackLogo)
 console.log('Post to sting: ', post.toString())
 
 console.log('JSON: ', json)
+console.log('XML: ', xml)
