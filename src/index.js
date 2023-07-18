cda2ad3d501db981c9d65e08094825d3cd0468b3
Artem Zhoglo <zhogloaa@yandex.ru>
2023-07-18 21:20:43 +0300

    Added alias and extensions in resolve

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import Post from './Post'
-import json from './assets/json'
-import xml from './assets/data.xml'
-import csv from './assets/data.csv'
-import WebpackLogo from './assets/webpack-logo.png'
-import './styles/styles.css'
+import Post from '@models/Post'
+import json from '@/assets/json'
+import xml from '@/assets/data.xml'
+import csv from '@/assets/data.csv'
+import WebpackLogo from '@/assets/webpack-logo.png'
+import '@/styles/styles.css'
 
 
 const post = new Post('Webpack Post Title', WebpackLogo)
